f54393741d0ed9a7362b628a4554c0173809f64c
Roksana St <160417317+Roksana-St@users.noreply.github.com>
2024-11-16 20:50:04 +0200

    hw2

--- src/server.js
+++ src/server.js
@@ -1,19 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino';
+import pinoHttp from 'pino-http';
 import contactsRouter from './routes/contacts.js';
 
-const app = express();
+export const setupServer = () => {
+  const app = express();
+  const logger = pino();
 
-app.use(express.json());
+  app.use(cors());
+  app.use(pinoHttp({ logger }));
 
-app.use('/contacts', contactsRouter);
+  app.use('/contacts', contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
-});
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 
-export default app;
+  return app;
+};
+export default setupServer;
