cd2ba302158d7ce29eee97a0477838cc509d6f6e
Roksana St <160417317+Roksana-St@users.noreply.github.com>
2025-01-02 22:15:33 +0200

    add clodinary

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,10 +1,24 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(process.cwd(), 'uploads');
-    cb(null, uploadPath);
+    
+    // Логування шляху
+    console.log('Шлях до директорії для завантаження:', uploadPath);
+
+    // Перевірка доступу до директорії
+    fs.access(uploadPath, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+      if (err) {
+        console.error('Директорія недоступна для запису:', uploadPath);
+        cb(new Error('Директорія недоступна для запису'));
+      } else {
+        console.log('Директорія доступна для запису:', uploadPath);
+        cb(null, uploadPath);
+      }
+    });
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = `${Date.now()}-${file.originalname}`;
@@ -12,7 +26,4 @@ const storage = multer.diskStorage({
   },
 });
 
-
 export const upload = multer({ storage });
-
-
