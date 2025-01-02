1e78772f8844c65f9f1f849e74fde686fa87ec5c
Roksana St <160417317+Roksana-St@users.noreply.github.com>
2025-01-02 22:21:05 +0200

    add clodinary

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,23 +1,27 @@
-import multer from 'multer';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(process.cwd(), 'uploads');
-    
-    // Логування шляху
-    console.log('Шлях до директорії для завантаження:', uploadPath);
 
-    // Перевірка доступу до директорії
-    fs.access(uploadPath, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+    // Перевірка існування директорії, якщо її немає - створюємо
+    fs.access(uploadPath, fs.constants.F_OK, (err) => {
       if (err) {
-        console.error('Директорія недоступна для запису:', uploadPath);
-        cb(new Error('Директорія недоступна для запису'));
-      } else {
-        console.log('Директорія доступна для запису:', uploadPath);
-        cb(null, uploadPath);
+        console.log('Директорія не існує, створюємо...');
+        fs.mkdirSync(uploadPath);  // Створюємо директорію
       }
+
+      // Перевірка прав на запис
+      fs.access(uploadPath, fs.constants.W_OK, (err) => {
+        if (err) {
+          console.error('Директорія недоступна для запису:', uploadPath);
+          cb(new Error('Директорія недоступна для запису'));
+        } else {
+          console.log('Директорія доступна для запису:', uploadPath);
+          cb(null, uploadPath);
+        }
+      });
     });
   },
   filename: (req, file, cb) => {
